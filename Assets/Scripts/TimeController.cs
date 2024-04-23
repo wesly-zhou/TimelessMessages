74aa4f3f539651bb108db87f721b2c43ef8c933b
wesly-zhou <weslyz1026@gmail.com>
2024-04-23 00:08:06 -0400

    Ability cooldown timer on time travel

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -4,6 +4,7 @@ using Pathfinding;
 using UnityEngine;
 using TMPro;
 using UnityEngine.Rendering;
+using UnityEngine.UI;
 
 public class TimeController : MonoBehaviour
 {
@@ -21,20 +22,22 @@ public class TimeController : MonoBehaviour
     private float transitionTime;
     public float duration = 1f; 
     public static bool operatable = true;
-    // private float t;
-    // public float t1;
-    // public float t2;
-    // public float t3;
-    // public float speed1;
-    // public float speed2;
     private static bool setupTime = true;
     public GameObject Enemy;
     public TextMeshProUGUI timerText;
     public static float remainingTime = 1220;
     public Volume volume;
 
+    public Image timerIcon;
+    public Text cooldownText;
+    public static float timerCooldown = 15f;
+    private static bool onCooldown = false;
+    private static float currentCooldown;
+
     void Start()
     {
+        timerIcon.fillAmount = 0;
+        cooldownText.text = "";
         transitionTime = 0;
         material = TimeLeapVFX.GetComponent<SpriteRenderer>().material;
         spriteRenderer = TimeLeapVFX.GetComponent<SpriteRenderer>();
@@ -78,6 +81,8 @@ public class TimeController : MonoBehaviour
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+
+        AbilityCooldown(ref currentCooldown, timerCooldown, ref onCooldown, timerIcon, cooldownText);
 
         if (startTransition1){
             transitionTime += Time.deltaTime;
@@ -142,12 +147,13 @@ public class TimeController : MonoBehaviour
         
     }
 
-    public void OnbuttonClick(){
-        if (setupTime)
+    public void OnbuttonClick() {
+        if (setupTime && !onCooldown)
         {   
             print("Start1: " + startTransition1);
             print("Start2: " + startTransition2);
-        
+            onCooldown = true;
+            currentCooldown = timerCooldown;
             setupTime = false;
             transitionTime = 0;
             GameObject.FindGameObjectWithTag("Player").GetComponentInChildren<Animator>().SetInteger("direction", 3);
@@ -165,6 +171,26 @@ public class TimeController : MonoBehaviour
             TimeLeapVFX.GetComponent<SpriteRenderer>().enabled = true;
             StartCoroutine(TimeTravel());
             // transitionTime = 0;
+        }
+    }
+
+    private void AbilityCooldown(ref float currentCooldown, float maxCooldown, ref bool onCooldown, Image skillImage, Text skillText) {
+        if (onCooldown) {
+            currentCooldown -= Time.deltaTime;
+            if (currentCooldown <= 0) {
+                onCooldown = false;
+                currentCooldown = 0f;
+                if (skillImage != null) 
+                    skillImage.fillAmount = 0f;
+                if (skillText != null)
+                    skillText.text = "";
+            }
+            else {
+                if (skillImage != null)
+                    skillImage.fillAmount = currentCooldown / maxCooldown;
+                if (skillText != null)
+                    skillText.text = Mathf.Ceil(currentCooldown).ToString();
+            }
         }
     }
 }
