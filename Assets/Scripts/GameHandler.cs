79975bcbf5b74b89dc1996a628caae672682e8c6
wesly-zhou <weslyz1026@gmail.com>
2024-04-23 18:56:00 -0400

    Removed player physics

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -7,17 +7,22 @@ using UnityEngine.SceneManagement;
 public class GameHandler : MonoBehaviour
 {
     private InventoryManager inventoryManager;
+    public static bool introLoaded = false;
 
     private void Start() {
         inventoryManager = GameObject.Find("InventoryCanvas").GetComponent<InventoryManager>();
     }
 
     public void StartGame() {
-        SceneManager.LoadScene("IntroScene");
+        if (!introLoaded)
+            SceneManager.LoadScene("IntroScene");
+        else
+            SceneManager.LoadScene("ChemLab");
     }
 
     public void Main() {
         SceneManager.LoadScene("MainMenu");
+        introLoaded = true;
     }
 
     public void Credits() {
