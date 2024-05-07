1e292b642807808c8d14662f2c1ca6e28c192484
wesly-zhou <weslyz1026@gmail.com>
2024-05-07 15:45:53 -0400

    Update TimeController.cs

--- Assets/Scripts/TimeController.cs
+++ Assets/Scripts/TimeController.cs
@@ -88,11 +88,13 @@ public class TimeController : MonoBehaviour
     }
 
     void Update() {
-        if (remainingTime > 0)
+        if (remainingTime > 0) {
             remainingTime -= Time.deltaTime;
-        else if (remainingTime < 0)
+        }
+        else if (remainingTime < 0) {
             remainingTime = 0;
             SceneManager.LoadScene("LoseScene");
+        }
         int minutes = Mathf.FloorToInt(remainingTime / 60);
         int seconds = Mathf.FloorToInt(remainingTime % 60);
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
