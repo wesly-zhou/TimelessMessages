34c446aa32d29fb235058b8c803b4d499978cafc
wesly-zhou <weslyz1026@gmail.com>
2024-05-07 00:40:08 -0400

    Connected scenes

--- Assets/Scripts/FinalDoorController.cs
+++ Assets/Scripts/FinalDoorController.cs
@@ -1,25 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class FinalDoorController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public string newLevel;
     public GameObject TextBubble;
+    public GameObject secondTextBubble;
     public GameObject FinalDoorPanel;
     private bool interactable = false;
     public static bool isOpen = false;
     void Start()
     {
-        
+        FinalDoorPanel.SetActive(false);
+        TextBubble.SetActive(false);
+        secondTextBubble.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (interactable && Input.GetKeyDown(KeyCode.E)) {
+        if (isOpen) {
+            secondTextBubble.SetActive(true);
+        }
+        if (isOpen && Input.GetKeyDown(KeyCode.E)) {
+            SceneManager.LoadScene(newLevel);
+        }
+        if (!isOpen && interactable && Input.GetKeyDown(KeyCode.E)) {
             FinalDoorPanel.SetActive(true);
-            
             TextBubble.SetActive(false);
         }
     }
@@ -29,12 +39,15 @@ public class FinalDoorController : MonoBehaviour
             TextBubble.SetActive(true);
             interactable = true;
         }
+        if (other.gameObject.tag == "Player" && isOpen) {
+            secondTextBubble.SetActive(true);
+        }
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         if (other.gameObject.tag == "Player") {
             TextBubble.SetActive(false);
-            interactable = false;
+            secondTextBubble.SetActive(false);
         }
     }
 }
